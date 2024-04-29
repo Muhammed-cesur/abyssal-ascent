2b9c813c41cf9679477cdf798607d748052e6b5b
Furkan Acer <frukan4@gmail.com>
2024-04-29 22:46:06 +0300

    Level 2, UI and bugs fixed
    Level 2, UI and bugs fixed

---
+++ Assets/Script/UI/Canvas Manager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasManager : MonoBehaviour
+{
+    public GameObject ingameCanvas;
+    public GameObject pauseCanvas;
+
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                ResumeGame();
+            else
+                PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        Debug.Log("Oyun duraklatıldı.");
+        isPaused = true;
+        Time.timeScale = 0f; // Oyun zamanını duraklat
+        ingameCanvas.SetActive(false); // IngameCanvas'ı devre dışı bırak
+        pauseCanvas.SetActive(true); // PauseCanvas'ı aktif hale getir
+    }
+
+    public void ResumeGame()
+    {
+        Debug.Log("Oyun devam ettirildi.");
+        isPaused = false;
+        Time.timeScale = 1f; // Oyun zamanını normale döndür
+        pauseCanvas.SetActive(false); // PauseCanvas'ı devre dışı bırak
+        ingameCanvas.SetActive(true); // IngameCanvas'ı aktif hale getir
+    }
+}
